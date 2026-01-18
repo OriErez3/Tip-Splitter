61c2bb787b04d2e962f6dc17e703f69ed73a23a8
OriErez3 <orierez3@gmail.com>
2026-01-18 16:01:16 -0500

    Fixed another registration issue

--- Backend/user-auth/models/user.js
+++ Backend/user-auth/models/user.js
@@ -23,14 +23,9 @@ const UserSchema = new mongoose.Schema({
     
 });
 
-UserSchema.pre('save', async function (next){
-    try{
+UserSchema.pre('save', async function (){
     if (!this.isModified("password")) return;
     this.password = await bcrypt.hash(this.password, 10);
-    return next()
-    } catch(err){
-        return next(err);
-    }
 });
 
 module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
